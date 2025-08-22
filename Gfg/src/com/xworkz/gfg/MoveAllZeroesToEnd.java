275d88ace44d429f34f115a3ceaba316265ea848
Gayatri-k3 <gayatrik1029@gmail.com>
2025-08-22 14:13:18 +0530

    Move all zeroes to the end, Day 2

---
+++ Gfg/src/com/xworkz/gfg/MoveAllZeroesToEnd.java
@@ -0,0 +1,23 @@
+package com.xworkz.gfg;
+
+import java.util.Arrays;
+
+public class MoveAllZeroesToEnd {
+    static void toEnd(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                int temp = arr[i];
+                arr[i] = arr[count];
+                arr[count] = temp;
+                count++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr={2,0,5,0,6,0,6};
+        toEnd(arr);
+        System.out.println(Arrays.toString(arr)+" ");
+    }
+}
